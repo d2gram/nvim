284e539d21190c09e3bd1e1343c28d85d72af574
d2gram <d29ram@gmail.com>
2025-03-26 11:15:06 +0900

    cpp namespace

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -96,6 +96,7 @@ vim.api.nvim_create_autocmd("ColorScheme", {
             ["@lsp.type.method"] = { fg = get_default_fg("@variable"), italic = false },
             ["@lsp.mod.declaration"] = { fg = "#ffffff", italic = false },
             ["@lsp.mod.definition"] = { fg = "#ffffff", italic = false },
+            ["@lsp.type.namespace.cpp"] = { fg = "#ffffff", italic = false, bold = true },
         })
 
         -- 기타 하이라이트 설정
